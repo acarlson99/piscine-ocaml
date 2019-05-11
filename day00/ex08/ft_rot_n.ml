685e6a53b7cd897012a493bfa1db4d4bd5dee44a
Alexander <alexandercarlson99@gmail.com>
2019-05-11 16:16:40 -0700

    YEE

--- day00/ex08/ft_rot_n.ml
+++ day00/ex08/ft_rot_n.ml
@@ -1,12 +1,19 @@
 let ft_rot_n (n : int) (s : string) : string =
   let f n c =
-    if c >= 'a' && c <= 'z' then char_of_int ((int_of_char c + n) mod (int_of_char 'a' - int_of_char 'z') + int_of_char 'a')
-    else if c >= 'A' && c <= 'Z' then c
+    if c >= 'a' && c <= 'z' then let n = int_of_char c + n in
+                                 if n > int_of_char 'z' then char_of_int (n - 26)
+                                 else char_of_int n
+    else if c >= 'A' && c <= 'Z' then let n = int_of_char c + n in
+                                      if n > int_of_char 'Z' then char_of_int (n - 26)
+                                      else char_of_int n
     else c
   in
-  let p = f n
+  let p = f (n mod 26)
   in
   String.map p s
 
 let () =
-  print_endline (ft_rot_n 3 "abc!")
+  print_endline (ft_rot_n 39 "ABCZ!");
+  print_endline (ft_rot_n 13 "abcz!");
+  print_endline (ft_rot_n 1 (ft_rot_n 12 "abcz!"));
+  print_endline (ft_rot_n 13 "qPbqr EBG Nhgb")
