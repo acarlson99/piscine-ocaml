59baccd701470b21d27ea21d691e17c00d142ac4
John <bombblob@gmail.com>
2019-05-11 14:20:09 -0700

    yeet

---
+++ day00/ex07/ft_is_palindrome.ml
@@ -0,0 +1,18 @@
+let ft_is_palindrome (s : string) : bool =
+  let rec f s l i =
+    if l - i < i then true
+    else if s.[i] == s.[l - i] then f s l (i + 1)
+    else false
+  in
+  f s (String.length s - 1) 0
+
+let test s =
+  if ft_is_palindrome s then print_endline "YEP"
+  else print_endline "NOPE"
+
+let () =
+  test "abccba";
+  test "abcba";
+  test "bbcba";
+  test "a";
+  test "";
