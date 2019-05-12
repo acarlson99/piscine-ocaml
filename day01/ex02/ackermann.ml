f777239e7b46bc6ba585464be1e6afb88036a701
Alexander <alexandercarlson99@gmail.com>
2019-05-11 19:17:11 -0700

    Yeet

---
+++ day01/ex02/ackermann.ml
@@ -0,0 +1,13 @@
+let rec ackermann m n  =
+  match m, n with
+    0, _ -> n + 1
+  | _, 0 when m > 0 -> ackermann (m - 1) 1
+  | _, _ when m > 0 && n > 0 -> ackermann (m - 1) (ackermann m (n - 1))
+  | _, _ -> 0
+
+let () =
+  Printf.printf "%d\n" (ackermann 1 1);
+  Printf.printf "%d\n" (ackermann 1 2);
+  Printf.printf "%d\n" (ackermann 3 2);
+  Printf.printf "%d\n" (ackermann 3 4);
+  Printf.printf "%d\n" (ackermann 4 1);
