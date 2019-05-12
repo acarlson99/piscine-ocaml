5eb3a76b0f9952e8480b35f869d9db5977ba63aa
Alexander <alexandercarlson99@gmail.com>
2019-05-11 21:06:49 -0700

    Fixed tail recursion

--- day01/ex08/ft_sum.ml
+++ day01/ex08/ft_sum.ml
@@ -1,7 +1,10 @@
 let rec ft_sum (f : int -> float) (lo : int) (hi : int) : float =
-  if hi < lo then nan
-  else if hi == lo then f lo
-  else f lo +. ft_sum f (lo + 1) hi
+  let rec fn f lo hi total =
+    if hi < lo then nan
+    else if hi == lo then f lo +. total
+    else fn f (lo + 1) hi (total +. (f lo))
+  in
+  fn f lo hi 0.
 
 let () =
   Printf.printf "%f\n" (ft_sum (fun i -> float_of_int (i * i)) 1 10);
