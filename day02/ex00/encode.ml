42abfb751c223fb360c4770016671b3f2687c273
John <bombblob@gmail.com>
2019-05-12 01:37:10 -0700

    Fixed prototype

--- day02/ex00/encode.ml
+++ day02/ex00/encode.ml
@@ -1,5 +1,8 @@
-let encode (l : 'a list) (f : 'a) list =
-  []
+let encode (l : 'a list) : (int * 'a) list =
+  [ (3, 4) ]
 
 (* val encode : 'a list -> (int * 'a) list =
  *   0 *)
+
+let () =
+  List.iter (Printf.printf "%d ") [1; 2; 3; 4];
