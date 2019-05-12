d12f9c0ac9c72245ca455c4f5e4a5a97aba38ee1
Alexander <alexandercarlson99@gmail.com>
2019-05-12 15:47:09 -0700

    Yeet

--- day02/ex00/encode.ml
+++ day02/ex00/encode.ml
@@ -1,8 +1,26 @@
 let encode (l : 'a list) : (int * 'a) list =
-  [ (3, 4) ]
-
-(* val encode : 'a list -> (int * 'a) list =
- *   0 *)
+  let rec f (l : 'a list) (n : int) : (int * 'a) list =
+    match l with
+      [] -> []
+    | a :: b :: c when a == b -> f (b::c) (n + 1)
+    | a :: b :: c -> ((n, a) :: (f (b::c) 1))
+    | a :: b -> (n, a) :: (f b 1)
+  in
+  f l 1
 
 let () =
-  List.iter (Printf.printf "%d ") [1; 2; 3; 4];
+  let print_tup ((x,y) : (int * int)) : unit =
+    Printf.printf "(%i %i) " x y
+  in
+  Printf.printf "[ ";
+  List.iter print_tup (encode [1; 1; 2; 3; 4]);
+  Printf.printf "]\n";
+  let print_tup ((x,y) : (int * char)) : unit =
+    Printf.printf "(%i %c) " x y
+  in
+  Printf.printf "[ ";
+  List.iter print_tup (encode ['a'; 'a'; 'b'; 'b'; 'b']);
+  Printf.printf "]\n";
+  Printf.printf "[ ";
+  List.iter print_tup (encode []);
+  Printf.printf "]\n";
