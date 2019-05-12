fafe3828876d3df3b4b6449a628a68d3044cb3e3
Alexander <alexandercarlson99@gmail.com>
2019-05-11 21:01:14 -0700

    I mean 'it' 😅

--- day01/ex08/ft_sum.ml
+++ day01/ex08/ft_sum.ml
@@ -5,3 +5,5 @@ let rec ft_sum (f : int -> float) (lo : int) (hi : int) : float =
 
 let () =
   Printf.printf "%f\n" (ft_sum (fun i -> float_of_int (i * i)) 1 10);
+  Printf.printf "%f\n" (ft_sum (fun i -> ((float_of_int i) /. 2.0)) 1 10);
+  Printf.printf "%f\n" (ft_sum (fun i -> (float_of_int i)) 1 10);
