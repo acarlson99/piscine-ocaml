9c3fcec1e79ae727fd876b64f6bcb0101062f6a3
Alexander <alexandercarlson99@gmail.com>
2019-05-11 20:58:59 -0700

    Nailed him

---
+++ day01/ex08/ft_sum.ml
@@ -0,0 +1,7 @@
+let rec ft_sum (f : int -> float) (lo : int) (hi : int) : float =
+  if hi < lo then nan
+  else if hi == lo then f lo
+  else f lo +. ft_sum f (lo + 1) hi
+
+let () =
+  Printf.printf "%f\n" (ft_sum (fun i -> float_of_int (i * i)) 1 10);
