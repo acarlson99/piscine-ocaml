efe0efad520e2078f6bd978f8d6613b08f68fc86
Alexander <alexandercarlson99@gmail.com>
2019-05-13 14:45:14 -0700

    Yeet

--- day02/ex05/helix.ml
+++ day02/ex05/helix.ml
@@ -33,7 +33,7 @@ let generate_helix (n : int) : helix =
   if n < 1 then []
   else let rec f n l =
          if n == 0 then l
-         else f (n - 1) (generate_nucleotide (getChar (Random.int 5)) :: l)
+         else f (n - 1) (generate_nucleotide (getChar (Random.int 4)) :: l)
        in
        f n []
 
@@ -48,14 +48,27 @@ let rec helix_to_string (hx : helix) : string =
   in
   let rec f hx st sp =
     match hx with
-      x::xs when sp -> f xs (st ^ " " ^ x.p ^ "-" ^ x.d ^ "-" ^ getBase x.n) true
-    | x::xs -> f xs (st ^ x.p ^ "-" ^ x.d ^ "-" ^ getBase x.n) true
+      x::xs when sp -> f xs (st ^ "-" ^ getBase x.n) true
+    | x::xs -> f xs (st ^ getBase x.n) true
     | _ -> st
   in
   f hx "" false
 
 let complementary_helix (hx : helix) : helix =
-  generate_helix 1
+  let getComp n =
+    match n with
+      A -> 'T'
+    | T -> 'A'
+    | C -> 'G'
+    | G -> 'C'
+    | _ -> 'F'
+  in
+  let rec f hx =
+    match hx with
+      x::xs -> generate_nucleotide (getComp x.n) :: f xs
+    | _ -> []
+  in
+  f hx
 
 let () =
   let printBoi (nt : nucleotide) : unit =
@@ -72,6 +85,8 @@ let () =
       x::xs -> (printBoi x; printBois xs)
     | [] -> Printf.printf "[]\n"
   in
-  let l = generate_helix 5 in
+  let l = generate_helix 15 in
   printBois l;
-  Printf.printf "%s\n" (helix_to_string l)
+  Printf.printf "%s\n" (helix_to_string l);
+  Printf.printf "%s\n" (helix_to_string (complementary_helix l));
+  Printf.printf "%s\n" (helix_to_string (complementary_helix (complementary_helix l)))
