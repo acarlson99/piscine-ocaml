f3e9d0269dfed6b9cc82d62ec00bc7c36b232a93
John <bombblob@gmail.com>
2019-05-14 11:29:45 -0700

    Gottem coach

--- day03/ex00/ft_graphics.ml
+++ day03/ex00/ft_graphics.ml
@@ -1,3 +1,5 @@
+type 'a tree = Nil | Node of 'a * 'a tree * 'a tree
+
 let draw_square (x : int) (y : int) (size : int) : unit =
   let tlx = (x - (size / 2)) in
   let tly = (y - (size / 2)) in
@@ -7,16 +9,20 @@ let draw_square (x : int) (y : int) (size : int) : unit =
   Graphics.lineto tlx (tly + size);
   Graphics.lineto tlx tly
 
-type 'a tree = Nil | Node of 'a * 'a tree * 'a tree
-
-let draw_tree (t : 'a tree) : unit =
-  Graphics.lineto 0 0
+let draw_tree_node (t : 'a tree) : unit =
+  let rec f x y size div t =
+    match t with
+      Node (a,b,c) -> Graphics.moveto (x-size/2) y; Graphics.draw_string a; draw_square x y size;
+                      let d = (600 / div) in
+                      f (x - d) (y - size) size (div*2) b;
+                      f (x + d) (y - size) size (div*2) c;
+    | Nil -> Graphics.moveto (x-size/2) y; Graphics.draw_string "nil"; draw_square x y size
+  in
+  f 300 384 30 4 t
 
 let () =
   Graphics.open_graph " 600x400";
-  draw_square 100 100 50;
-  draw_square 0 0 50;
-  draw_square 25 25 50;
+  draw_tree_node (Node ("abc", Nil, (Node ("def", (Node ("ghi", Nil, Nil)), (Node ("jkl", Nil, Nil))))));
   let rec f () =
     f ()
   in
