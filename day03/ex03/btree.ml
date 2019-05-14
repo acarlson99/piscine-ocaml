a7ae02edbffc857f9b1ffe79969a155c810b829c
Alexander <alexandercarlson99@gmail.com>
2019-05-14 14:42:43 -0700

    YEE

---
+++ day03/ex03/btree.ml
@@ -0,0 +1,20 @@
+type 'a tree = Nil | Node of 'a * 'a tree * 'a tree
+
+let is_bst (t : 'a tree) : bool =
+  true
+
+let is_perfect (t : 'a tree) : bool =
+  true
+
+let is_balanced (t : 'a tree) : bool =
+  true
+
+let search_bst (v : 'a) (t : 'a tree) : bool =
+  true
+
+let add_bst (v : 'a) (t : 'a tree) : 'a tree =
+  Nil
+
+let delete_bst (v : 'a) (t : 'a tree) : 'a tree =
+  Nil
+
