385d9942fa1d5a6efa6dec18fb1c7be9d156a533
John <bombblob@gmail.com>
2019-05-14 00:00:43 -0700

    WHEE

--- day03/ex00/ft_graphics.ml
+++ day03/ex00/ft_graphics.ml
@@ -7,9 +7,17 @@ let draw_square (x : int) (y : int) (size : int) : unit =
   Graphics.lineto tlx (tly + size);
   Graphics.lineto tlx tly
 
+type 'a tree = Nil | Node of 'a * 'a tree * 'a tree
+
+let draw_tree (t : 'a tree) : unit =
+  Graphics.lineto 0 0
+
 let () =
   Graphics.open_graph " 600x400";
   draw_square 100 100 50;
-  while true do
-    Printf.printf ""
-  done
+  draw_square 0 0 50;
+  draw_square 25 25 50;
+  let rec f () =
+    f ()
+  in
+  f ()
