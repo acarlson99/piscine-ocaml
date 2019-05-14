6d8808d637a0873f0634bfc993ec36b90e47993f
John <bombblob@gmail.com>
2019-05-13 23:41:55 -0700

    We got some stuff i guess

--- day03/ex00/ft_graphics.ml
+++ day03/ex00/ft_graphics.ml
@@ -1,2 +1,15 @@
+let draw_square (x : int) (y : int) (size : int) : unit =
+  let tlx = (x - (size / 2)) in
+  let tly = (y - (size / 2)) in
+  Graphics.moveto tlx tly;
+  Graphics.lineto (tlx + size) tly;
+  Graphics.lineto (tlx + size) (tly + size);
+  Graphics.lineto tlx (tly + size);
+  Graphics.lineto tlx tly
+
 let () =
-  Graphics.open_graph "A";
+  Graphics.open_graph " 600x400";
+  draw_square 100 100 50;
+  while true do
+    Printf.printf ""
+  done
