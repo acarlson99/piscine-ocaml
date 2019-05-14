cbcb9eccb8c264438fdcfe99a8c84e54bbb51922
Alexander <alexandercarlson99@gmail.com>
2019-05-14 14:33:40 -0700

    Gottem

--- day03/ex02/cipher.ml
+++ day03/ex02/cipher.ml
@@ -1,2 +1,24 @@
-let () =
-  Uncipher.f 12
+let rec caesar (s : string) (n : int) : string =
+  let ro n c =
+    match c with
+      'A' .. 'Z' -> char_of_int ((int_of_char c - (int_of_char 'A') + n) mod 26 + (int_of_char 'A'))
+    | 'a' .. 'z' -> char_of_int ((int_of_char c - (int_of_char 'a') + n) mod 26 + (int_of_char 'a'))
+    | _ -> c
+  in
+  let f n =
+    if n == 0 then s
+    else String.map (ro n) s
+  in
+  if n < 0 then caesar s (n + 26)
+  else f (n mod 26)
+
+let rot42 (s : string) : string =
+  caesar s 42
+
+let xor (s : string) (n : int) : string =
+  String.map (fun c -> char_of_int ((int_of_char c) lxor n)) s
+
+let rec ft_crypt (s : string) (f : (string -> string) list) : string =
+  match f with
+  | x::xs -> ft_crypt (x s) xs
+  | _ -> s
--- day03/ex02/uncipher.ml
+++ day03/ex02/uncipher.ml
@@ -1,2 +1,23 @@
-let f a =
-  Printf.printf "%d\n" a
+let uncaesar (s : string) (n : int) : string =
+  Cipher.caesar s (-n)
+
+let unrot42 (s : string) : string =
+  uncaesar s 42
+
+let rec ft_uncrypt (s : string) (f : (string -> string) list) : string =
+  match f with
+  | x::xs -> ft_uncrypt (x s) xs
+  | _ -> s
+
+let () =
+  print_endline (Cipher.caesar "aBcDeF123" (-13));
+  print_endline (Cipher.caesar "aBcDeF123" 13);
+  print_endline (Cipher.rot42 "abczF!");
+  print_endline (unrot42 (Cipher.rot42 "abczF!"));
+  print_endline (Cipher.caesar "abczF!" 1);
+  print_endline (uncaesar (Cipher.caesar "abczF!" 1) 1);
+  print_endline (Cipher.xor "abcdef !" 12);
+  print_endline (Cipher.xor (Cipher.xor "abcdef !" 12) 12);
+  let cryp = Cipher.ft_crypt "abcdef" [Cipher.rot42; Cipher.rot42; (fun s -> Cipher.xor s 2)] in
+  print_endline cryp;
+  print_endline (ft_uncrypt cryp [(fun s -> Cipher.xor s 2); unrot42; unrot42])
