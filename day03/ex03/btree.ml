5cf78fea9725f118211785ad827b602db83f58c6
John <bombblob@gmail.com>
2019-05-15 00:18:23 -0700

    Finally

--- day03/ex03/btree.ml
+++ day03/ex03/btree.ml
@@ -1,7 +1,15 @@
 type 'a tree = Nil | Node of 'a * 'a tree * 'a tree
 
-let is_bst (t : 'a tree) : bool =
-  true
+let rec is_bst (t : 'a tree) : bool =
+  let rec f (t : 'a tree) (mx : 'a option) (mn : 'a option) =
+    match t with
+      Node (a,b,c) ->
+       if mx != None && Some a > mx then false
+       else if mn != None && Some a < mn then false
+       else f b (Some a) mn && f c mx (Some a)
+    | Nil -> true
+  in
+  f t None None
 
 let is_perfect (t : 'a tree) : bool =
   true
@@ -17,3 +25,9 @@ let add_bst (v : 'a) (t : 'a tree) : 'a tree =
 
 let delete_bst (v : 'a) (t : 'a tree) : 'a tree =
   Nil
+
+let () =
+  let t = Node (10, Node (7, Nil, Node (42, Nil, Nil)), Node (11, Nil, Node (12, Nil, Nil))) in (* false *)
+  Printf.printf "%b\n" (is_bst t);
+  let t = Node (10, Node (7, Nil, Node (9, Nil, Nil)), Node (11, Nil, Node (12, Nil, Nil))) in (* true *)
+  Printf.printf "%b\n" (is_bst t);
