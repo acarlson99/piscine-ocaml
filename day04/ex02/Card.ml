864445f341c9c28f8883ceff612091397d4b1426
John <bombblob@gmail.com>
2019-05-16 00:33:52 -0700

    Fixed

--- day04/ex02/Card.ml
+++ day04/ex02/Card.ml
@@ -106,17 +106,21 @@ let newCard (v : Value.t) (c : Color.t) : t =
 
 let all = List.rev (List.fold_left (fun a cl -> List.fold_left (fun ab vl -> (vl,cl) :: ab) a Value.all) [] Color.all)
 
-let allSpades   = List.filter (fun a -> getValue a = Color.Spade) all
-let allHearts   = List.filter (fun a -> getValue a = Color.Heart) all
-let allDiamonds = List.filter (fun a -> getValue a = Color.Diamond) all
-let allClubs    = List.filter (fun a -> getValue a = Color.Club) all
+(* let allSpades = List.filter (fun a -> match a with (_,v) -> v = Color.Spade) all
+ * let allHearts = List.filter (fun a -> match a with (_,v) -> v = Color.Heart) all
+ * let allDiamonds = List.filter (fun a -> match a with (_,v) -> v = Color.Diamond) all
+ * let allClubs = List.filter (fun a -> match a with (_,v) -> v = Color.Club) all *)
 
 let getValue (c : t) : Value.t =
-  match b with
-    (_,v) -> v
+  match c with
+    (v,_) -> v
            
 let getColor (c : t) : Color.t =
-  match b with
-    (c,_) -> c
+  match c with
+    (_,c) -> c
 
                
+let allSpades   = List.filter (fun a -> getColor a = Color.Spade) all
+let allHearts   = List.filter (fun a -> getColor a = Color.Heart) all
+let allDiamonds = List.filter (fun a -> getColor a = Color.Diamond) all
+let allClubs    = List.filter (fun a -> getColor a = Color.Club) all
