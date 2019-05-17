70b43a5591cad58de02add2ffa0d930b7aeb9688
John <bombblob@gmail.com>
2019-05-16 21:34:33 -0700

    Yeet

--- day05/ex03/jokes.ml
+++ day05/ex03/jokes.ml
@@ -5,15 +5,14 @@ let () =
   else let addToArr (arr : j) (str : string) : unit =
          arr.content <- Array.append arr.content [|str|]
        in
-       let a = {content=[| |]} in
-
-
-       let ic = open_in (Sys.argv.(1)) in
-       try
-         let line = input_line ic in
-         print_endline line;
-         addToArr a line;
-         print_endline (a.content.(Random.int (Array.length a.content)));
-       with e ->
-         close_in_noerr ic;
-         print_endline "Error opening file";
+       try let ic = open_in (Sys.argv.(1)) in
+           try let a = {content=[| |]} in
+               try while true do
+                     addToArr a (input_line ic);
+                   done
+               with End_of_file -> for n=0 to 20 do
+                                     print_endline (a.content.(Random.int (Array.length a.content)))
+                                   done
+           with e -> close_in_noerr ic;
+                     print_endline "Error opening file";
+       with e -> print_endline "Error opening file";
