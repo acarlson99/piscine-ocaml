d7a282254b52e6cbbd61c2c72ae22678fc472d10
acarlson99 <acarlson99@gmail.com>
2019-05-17 02:44:23 +0000

    Naisu

--- day05/ex00/micronap.ml
+++ day05/ex00/micronap.ml
@@ -1,9 +1,10 @@
-(* let my_sleep () = Unix.sleep 1 *)
+(* ocamlbuild -lib unix micronap.native *)
+
+let my_sleep () = Unix.sleep 1
 
 let () =
-  if Array.length Sys.argv != 2 then print_endline "Invalid number of arguments"
+  if Array.length Sys.argv != 2 then exit 69
   else
-    Printf.printf "%s\n" (Array.get Sys.argv 1);
-  (* for n = 0 to int_of_string (Array.get Sys.argv 1) do
-   *   my_sleep ()
-   * done *)
+    for n = 1 to (try int_of_string (Array.get Sys.argv 1) with e -> exit 69) do
+      my_sleep ()
+    done
---
+++ day05/ex01/ft_ref.ml
@@ -0,0 +1,23 @@
+type 'a ft_ref = { mutable contents : 'a }
+
+let return (n : 'a) : 'a ft_ref =
+  { contents = n }
+
+let get (r : 'a ft_ref) : 'a =
+  r.contents
+
+let set (r : 'a ft_ref) (n : 'a) : unit =
+  r.contents <- n
+
+let bind (r : 'a ft_ref) (f : ('a -> 'b ft_ref)) : 'b ft_ref =
+  f (get r)
+
+let () =
+  let a = return 420 in
+  Printf.printf "%d\n" (get a);
+  set a 42;
+  Printf.printf "%d\n" (get a);
+  let b = bind a (fun x -> return "abcdef") in
+  Printf.printf "%s\n" (get b);
+  set b "ghijkl";
+  Printf.printf "%s\n" (get b);
