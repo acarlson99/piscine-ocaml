33b92ac664d1bef5d3ccae4ae77a102276ccc946
acarlson99 <acarlson99@gmail.com>
2019-05-17 03:02:55 +0000

    End me

---
+++ day05/ex02/jokes.ml
@@ -0,0 +1,22 @@
+let () =
+  let jokes_arr = [|"Python2"; "My life"; "lol I'm having a jolly time"; "*chuckles* Well actually"; "People"; "Hehe nice"|] in
+  print_endline (jokes_arr.(Random.int (Array.length jokes_arr)));
+  print_endline (jokes_arr.(Random.int (Array.length jokes_arr)));
+  print_endline (jokes_arr.(Random.int (Array.length jokes_arr)));
+  print_endline (jokes_arr.(Random.int (Array.length jokes_arr)));
+  print_endline (jokes_arr.(Random.int (Array.length jokes_arr)));
+  print_endline (jokes_arr.(Random.int (Array.length jokes_arr)));
+  print_endline (jokes_arr.(Random.int (Array.length jokes_arr)));
+  print_endline (jokes_arr.(Random.int (Array.length jokes_arr)));
+  print_endline (jokes_arr.(Random.int (Array.length jokes_arr)));
+  print_endline (jokes_arr.(Random.int (Array.length jokes_arr)));
+  print_endline (jokes_arr.(Random.int (Array.length jokes_arr)));
+  print_endline (jokes_arr.(Random.int (Array.length jokes_arr)));
+  print_endline (jokes_arr.(Random.int (Array.length jokes_arr)));
+  print_endline (jokes_arr.(Random.int (Array.length jokes_arr)));
+  print_endline (jokes_arr.(Random.int (Array.length jokes_arr)));
+  print_endline (jokes_arr.(Random.int (Array.length jokes_arr)));
+  print_endline (jokes_arr.(Random.int (Array.length jokes_arr)));
+  print_endline (jokes_arr.(Random.int (Array.length jokes_arr)));
+  print_endline (jokes_arr.(Random.int (Array.length jokes_arr)));
+  print_endline (jokes_arr.(Random.int (Array.length jokes_arr)));
