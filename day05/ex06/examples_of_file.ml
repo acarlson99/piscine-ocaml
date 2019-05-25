06b56ae2585058a5e6dea617a1310b484d3e5a62
Alexander <alexandercarlson99@gmail.com>
2019-05-25 16:22:52 -0700

    My god

--- day05/ex06/examples_of_file.ml
+++ day05/ex06/examples_of_file.ml
@@ -3,17 +3,23 @@ type j = { mutable content : (float array * string) list }
 (* TODO: finish *)
 
 let examples_of_file (filepath : string) : (float array * string) list =
-  let addToList (lst : j) (str : string) : unit =
-    lst.content <- lst.content @ [(Array.of_list (List.map float_of_string (String.split_on_char ',' str)), "g")]
+  let a = {content=[]} in
+  let addToList (str : string) : unit =
+    let l = String.split_on_char ',' str in
+    a.content <- a.content @ [(Array.of_list (List.mapi (fun i x -> if (i+1) = List.length l then 0.0 else float_of_string x) l), "g")]
+                               (* lst.content <- lst.content @ [(Array.of_list (List.map float_of_string (String.split_on_char ',' str)), "g")] *)
   in
   try let ic = open_in (filepath) in
-      let a = {content=[]} in
       try while true do
-            addToList a (input_line ic);
-          done
-      with End_of_file -> print_endline "EOF";
-                          j.content
-  with e -> print_endline "Unable to open file"
+            addToList (input_line ic);
+          done;
+          a.content
+      with End_of_file -> a.content
+  with e -> print_endline "Unable to open file";
+            Printf.eprintf "ERROR %s %s\n" (Printexc.to_string e) (Printexc.get_backtrace ());
+            []
 
 let () =
-  List.iter (fun x -> Array.iter (Printf.printf "%f ") (fst x); Printf.printf "%s\n" (snd x)) (examples_of_file "./ionosphere.data")
+  if Array.length Sys.argv != 2 then Printf.printf "Enter a file\n"
+  else let l = examples_of_file Sys.argv.(1) in
+       List.iter (fun x -> Array.iter (Printf.printf "%f ") (fst x); Printf.printf "%s\n" (snd x)) l
