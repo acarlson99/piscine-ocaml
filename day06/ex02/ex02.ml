4ae7f6f5fc7160689736f7610c263c7bcc572484
Alexander <alexandercarlson99@gmail.com>
2019-05-25 18:51:47 -0700

    Gottem

--- day06/ex02/ex02.ml
+++ day06/ex02/ex02.ml
@@ -1,8 +1,19 @@
 module type PAIR = sig val pair : (int * int) end
 module type VAL = sig val x : int end
 
-(* TODO: FIX ME !!! *)
+(* BEGIN *)
 
+module type MAKEPROJECTION = sig val x : int end
+
+module MakeFst (P : PAIR) : MAKEPROJECTION = struct
+  let x = fst P.pair
+end
+
+module MakeSnd (P : PAIR) : MAKEPROJECTION = struct
+  let x = snd P.pair
+end
+
+(* END *)
 
 module Pair : PAIR = struct let pair = ( 21, 42 ) end
 
