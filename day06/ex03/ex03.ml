3398c6c9d7a5a7e093c736bab7ce7a078ed8e745
Alexander <alexandercarlson99@gmail.com>
2019-05-25 19:14:58 -0700

    Dear god why

---
+++ day06/ex03/ex03.ml
@@ -0,0 +1,13 @@
+module type FIXED = sig
+  type t = int
+end
+
+module Fixed4 : FIXED = Make (struct let bits = 4 end)
+module Fixed8 : FIXED = Make (struct let bits = 8 end)
+
+let () =
+  let x8 = Fixed8.of_float 21.10 in
+  let y8 = Fixed8.of_float 21.32 in
+  let r8 = Fixed8.add x8 y8 in
+  print_endline (Fixed8.to_string r8);
+  Fixed4.foreach (Fixed4.zero) (Fixed4.one) (fun f -> print_endline (Fixed4.to_string f))
