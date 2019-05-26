30750e2ae0dbc26e4472e7cc486786eedc535d51
Alexander <alexandercarlson99@gmail.com>
2019-05-25 21:31:22 -0700

    Why

--- day06/ex03/ex03.ml
+++ day06/ex03/ex03.ml
@@ -1,11 +1,27 @@
 module type FIXED = sig
-  type t = int
-
-  val zero : int
-end
-
-module Make (F : FIXED) = struct
-  let x = F.zero
+  type t
+  val of_float : float -> t
+  val of_int : int -> t
+  val to_float : t -> float
+  val to_int : t -> int
+  val to_string : t -> string
+  val zero : t
+  val one : t
+  val succ : t -> t
+  val pred : t -> t
+  val min : t -> t -> t
+  val max : t -> t -> t
+  val gth : t -> t -> bool
+  val lth : t -> t -> bool
+  val gte : t -> t -> bool
+  val lte : t -> t -> bool
+  val eqp : t -> t -> bool
+  val eqs : t -> t -> bool
+  val add : t -> t -> t
+  val sub : t -> t -> t
+  val mul : t -> t -> t
+  val div : t -> t -> t
+  val foreach : t -> t -> (t -> unit) -> unit
 end
 
 module Fixed4 : FIXED = Make (struct let bits = 4 end)
@@ -13,7 +29,11 @@ module Fixed8 : FIXED = Make (struct let bits = 8 end)
 
 let () =
   let x8 = Fixed8.of_float 21.10 in
-  let y8 = Fixed8.of_float 21.32 in
-  let r8 = Fixed8.add x8 y8 in
-  print_endline (Fixed8.to_string r8);
-  Fixed4.foreach (Fixed4.zero) (Fixed4.one) (fun f -> print_endline (Fixed4.to_string f))
+  Printf.printf "%s\n" Fixed8.to_string x8
+
+                (* let () =
+                 *   let x8 = Fixed8.of_float 21.10 in
+                 *   let y8 = Fixed8.of_float 21.32 in
+                 *   let r8 = Fixed8.add x8 y8 in
+                 *   print_endline (Fixed8.to_string r8);
+                 *   Fixed4.foreach (Fixed4.zero) (Fixed4.one) (fun f -> print_endline (Fixed4.to_string f)) *)
