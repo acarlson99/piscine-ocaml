ac9e99744a6f5e36ee1cd49d3242917e9dd1b945
Alexander <alexandercarlson99@gmail.com>
2019-05-26 14:10:12 -0700

    Go commit die

--- day07/ex00/main.ml
+++ day07/ex00/main.ml
@@ -1,3 +1,5 @@
 let () =
   let p = new People.people "Rose" in
   print_endline p#to_string;
+  let pr = new People.people in
+  (pr "Jim")#die
--- day07/ex00/people.ml
+++ day07/ex00/people.ml
@@ -2,7 +2,7 @@ class people name =
 object (self)
   (* val name : string = n *)
   val hp : int = 100
-  initializer (print_endline ("Oh the pain of being alive " ^ self#to_string))
+  initializer (print_endline ("Oh the pain of being alive: " ^ self#to_string))
   method to_string = name ^ " " ^ (string_of_int hp)
   method talk = print_endline ("I'm " ^ name ^ "! Do you know the Doctor?")
   method die = print_endline "Aaaarghh!"
