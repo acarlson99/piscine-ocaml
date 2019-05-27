3c0b8f6ed29218208710621e14e59441ad603a40
Alexander <alexandercarlson99@gmail.com>
2019-05-27 16:32:39 -0700

    🅱️

---
+++ day08/ex02/alkane.ml
@@ -0,0 +1,8 @@
+class alkane (n : int) =
+object(self)
+  val name : string = ""
+  val formula : string = ""
+
+  method to_string : string = ""
+  method equals a : bool = true
+end
---
+++ day08/ex02/main.ml
@@ -0,0 +1,38 @@
+let () =
+  let h = new Atom.hydrogen in
+  let o = new Atom.oxygen in
+  let c = new Atom.carbon in
+  let tm = new Atom.thulium in
+  let mo = new Atom.molybdenum in
+  print_endline h#to_string;
+  print_endline @@ string_of_bool @@ h#equals h;
+  print_endline c#to_string;
+  print_endline @@ string_of_bool @@ c#equals h;
+  print_endline o#to_string;
+  print_endline @@ string_of_bool @@ o#equals c;
+  print_endline tm#to_string;
+  print_endline @@ string_of_bool @@ tm#equals tm;
+  print_endline mo#to_string;
+  print_endline mo#to_string_verbose;
+  print_endline @@ string_of_bool @@ mo#equals c;
+
+  let tnt = new Molecule.trinitrotoluene in
+  print_endline tnt#to_string;
+
+  let water = new Molecule.water in
+  print_endline water#to_string;
+
+  let carbon_dioxyde = new Molecule.carbon_dioxyde in
+  print_endline carbon_dioxyde#to_string;
+
+  let nitrous_oxide = new Molecule.nitrous_oxide in
+  print_endline nitrous_oxide#to_string;
+
+  let glucose = new Molecule.glucose in
+  print_endline glucose#to_string;
+
+  let calcium_oxide = new Molecule.calcium_oxide in
+  print_endline calcium_oxide#to_string;
+
+  print_endline @@ string_of_bool @@ calcium_oxide#equals calcium_oxide;
+  print_endline @@ string_of_bool @@ calcium_oxide#equals glucose;
