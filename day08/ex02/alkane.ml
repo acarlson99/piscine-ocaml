050a4a1922b1db1929fc60756d994f2e5bc128f6
Alexander <alexandercarlson99@gmail.com>
2019-05-27 17:45:16 -0700

    Noice

--- day08/ex02/alkane.ml
+++ day08/ex02/alkane.ml
@@ -1,8 +1,70 @@
 class alkane (n : int) =
 object(self)
-  val name : string = ""
-  val formula : string = ""
+  val name : string = (match n with
+                         1  -> "meth"
+                       | 2  -> "eth"
+                       | 3  -> "prop"
+                       | 4  -> "but"
+                       | 5  -> "pent"
+                       | 6  -> "hex"
+                       | 7  -> "sept"
+                       | 8  -> "oct"
+                       | 9  -> "non"
+                       | 10 -> "dec"
+                       | 11 -> "undec"
+                       | 12 -> "dodec"
+                       | _  -> "NOOOOOOOOOOOOOOOOOOOPELOL") ^ "ane"
+  val formula : string = "C"^(if n = 1 then "" else string_of_int n)^"H"^(string_of_int @@ 2 * n + 2)
 
-  method to_string : string = ""
-  method equals a : bool = true
+  method to_string : string = formula
+  method getName : string = name
+  method equals (a : alkane) : bool = self#to_string = a#to_string
+end
+
+class methane =
+object inherit alkane 1
+end
+
+class ethane =
+object inherit alkane 2
+end
+
+class propane =
+object inherit alkane 3
+end
+
+class butane =
+object inherit alkane 4
+end
+
+class pentane =
+object inherit alkane 5
+end
+
+class hexane =
+object inherit alkane 6
+end
+
+class septane =
+object inherit alkane 7
+end
+
+class octane =
+object inherit alkane 8
+end
+
+class nonane =
+object inherit alkane 9
+end
+
+class decane =
+object inherit alkane 10
+end
+
+class undecane =
+object inherit alkane 11
+end
+
+class dodecane =
+object inherit alkane 12
 end
--- day08/ex02/main.ml
+++ day08/ex02/main.ml
@@ -18,21 +18,55 @@ let () =
 
   let tnt = new Molecule.trinitrotoluene in
   print_endline tnt#to_string;
-
   let water = new Molecule.water in
   print_endline water#to_string;
-
   let carbon_dioxyde = new Molecule.carbon_dioxyde in
   print_endline carbon_dioxyde#to_string;
-
   let nitrous_oxide = new Molecule.nitrous_oxide in
   print_endline nitrous_oxide#to_string;
-
   let glucose = new Molecule.glucose in
   print_endline glucose#to_string;
-
   let calcium_oxide = new Molecule.calcium_oxide in
   print_endline calcium_oxide#to_string;
-
   print_endline @@ string_of_bool @@ calcium_oxide#equals calcium_oxide;
   print_endline @@ string_of_bool @@ calcium_oxide#equals glucose;
+
+  let methane = new Alkane.methane in
+  print_endline methane#to_string;
+  print_endline methane#getName;
+  let ethane = new Alkane.ethane in
+  print_endline ethane#to_string;
+  print_endline ethane#getName;
+  let propane = new Alkane.propane in
+  print_endline propane#to_string;
+  print_endline propane#getName;
+  let butane = new Alkane.butane in
+  print_endline butane#to_string;
+  print_endline butane#getName;
+  let pentane = new Alkane.pentane in
+  print_endline pentane#to_string;
+  print_endline pentane#getName;
+  let hexane = new Alkane.hexane in
+  print_endline hexane#to_string;
+  print_endline hexane#getName;
+  let septane = new Alkane.septane in
+  print_endline septane#to_string;
+  print_endline septane#getName;
+  let octane = new Alkane.octane in
+  print_endline octane#to_string;
+  print_endline octane#getName;
+  let nonane = new Alkane.nonane in
+  print_endline nonane#to_string;
+  print_endline nonane#getName;
+  let decane = new Alkane.decane in
+  print_endline decane#to_string;
+  print_endline decane#getName;
+  let undecane = new Alkane.undecane in
+  print_endline undecane#to_string;
+  print_endline undecane#getName;
+  let dodecane = new Alkane.dodecane in
+  print_endline dodecane#to_string;
+  print_endline dodecane#getName;
+  let thing = new Alkane.alkane 209 in
+  print_endline thing#to_string;
+  print_endline thing#getName;
